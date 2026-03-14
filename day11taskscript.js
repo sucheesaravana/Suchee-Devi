9a2f34240f1c4e5c3d0f358544c118e945494978
sucheesaravana <sucheekarmuhilan@gmail.com>
2026-03-14 12:14:22 +0530

    Add files via upload

---
+++ day11taskscript.js
@@ -0,0 +1,32 @@
+
+const COMPANY_NAME = "Innovate Corp";
+console.log("Company Name:", COMPANY_NAME);
+
+let projectCount = 5;
+console.log("Initial project count:", projectCount);
+
+console.log("Type of projectCount:", typeof projectCount);
+
+projectCount = 6;
+console.log("Updated project count:", projectCount);
+
+const hasPermission = true;
+console.log("Has permission:", hasPermission);
+
+let futureFeature;
+console.log("futureFeature value:", futureFeature);
+console.log("futureFeature type:", typeof futureFeature);
+
+const WEEKDAYS = ["Mon", "Tue", "Wed"];
+console.log("Weekdays:", WEEKDAYS);
+
+console.log("First weekday:", WEEKDAYS[0]);
+
+
+const widget = {
+    color: "blue",
+    price: 25
+};
+console.log("Widget object:", widget);
+
+console.log(`Company ${COMPANY_NAME} is currently handling ${projectCount} projects.`);
